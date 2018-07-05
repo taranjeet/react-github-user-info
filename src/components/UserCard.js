2594317f17e4bef3fa97967470a334bb80963716
Taranjeet Singh <reachtotj@gmail.com>
2018-07-05 15:28:06 +0530

    update image width

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -8,9 +8,15 @@ const UserCard = ({fullName, publicRepos, followers, following, avatarUrl}) => {
             <div className="row">
                 <div className="col-8 offset-sm-2">
                 <Card>
-                <CardImg top width="100%" src={avatarUrl} alt={fullName} />
+                <div className="row">
+                    <div className="col-4">
+                        <CardImg top width="100%" src={avatarUrl} style={{ width: '200px' }} alt={fullName} />
+                    </div>
+                    <div className="col-4">
+                        {fullName}
+                    </div>
+                </div>
                 <CardBody>
-                    <CardTitle>{fullName}</CardTitle>
                     <CardSubtitle>
                         <div className="row">
                             <div className="col-3">
